3d45252f98ea5a9b265a6a5af70a6372eca34519
Nomi <noman1997@hotmail.se>
2019-08-05 21:01:29 +0200

    Updates

---
+++ precache-manifest.7043db47ac2dc1c0b319d4f92b0e2922.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "767e76165743a0a9a4dbecff00f51101",
+    "url": "/CV/index.html"
+  },
+  {
+    "revision": "835d2a9fef3b5bd2110d",
+    "url": "/CV/static/css/main.65475bec.chunk.css"
+  },
+  {
+    "revision": "cf109a7fb36f0f64e769",
+    "url": "/CV/static/js/2.4b95b049.chunk.js"
+  },
+  {
+    "revision": "835d2a9fef3b5bd2110d",
+    "url": "/CV/static/js/main.026b8b80.chunk.js"
+  },
+  {
+    "revision": "21043815baf811eb5681",
+    "url": "/CV/static/js/runtime~main.50de0404.js"
+  },
+  {
+    "revision": "f6bcf4a039dced55ea810ceab7192258",
+    "url": "/CV/static/media/Noman.f6bcf4a0.jpg"
+  }
+]);
